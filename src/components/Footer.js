3a666feec38e6c1f0f05f7d5ca41b9d46125f817
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2023-07-02 21:06:23 -0700

    deleted unused photos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/github.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import github from "../assets/img/github.svg";
+import facebook from "../assets/img/facebook.svg";
+import instagram from "../assets/img/instagram.svg";
 
 export const Footer = () => {
   return (
@@ -15,13 +14,13 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://github.com/Fox-Robotics">
-                <img src={navIcon1} alt="Icon" />
+                <img src={github} alt="Icon" />
               </a>
               <a href="https://www.facebook.com/FoxRobotics">
-                <img src={navIcon2} alt="Icon" />
+                <img src={facebook} alt="Icon" />
               </a>
               <a href="https://www.instagram.com/foxrobs/">
-                <img src={navIcon3} alt="Icon" />
+                <img src={instagram} alt="Icon" />
               </a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
