20b6d6441c0bf618390071c4348cb3fe2c86303c
Fox Robotics <foxrobotics@cetys.edu.mx>
2023-07-10 03:55:43 -0700

    Update RoboticsProjectList.js

--- src/components/RoboticsProjectList.js
+++ src/components/RoboticsProjectList.js
@@ -1,6 +1,12 @@
 import LineFollowerImg1 from "../assets/img/LineFollowerV1,1.jpg";
 import LineFollowerImg2 from "../assets/img/LineFollowerV1,2.jpg";
 import LineFollowerImg3 from "../assets/img/LineFollowerV1,3.jpg";
+import Follower V2 from "../assets/img/LineFollowerV1,1.jpg";
+import LineFollowerImg2 from "../assets/img/LineFollowerV1,2.jpg";
+import LineFollowerImg3 from "../assets/img/LineFollowerV1,3.jpg";
+import LineFollowerImg1 from "../assets/img/LineFollowerV1,1.jpg";
+import LineFollowerImg2 from "../assets/img/LineFollowerV1,2.jpg";
+import LineFollowerImg3 from "../assets/img/LineFollowerV1,3.jpg";
 import UTT from "../assets/videos/UTT.mp4";
 
 /*
@@ -45,20 +51,16 @@ const Roboticsprojects = [
     title: "Line Follower V2",
     veryShortDescription: "V2",
     landingDescription:
-      "2Here at Fox Robotics, we are looking for opportunities to change the way you look at technologies. We are innovators, we are disruptors.",
-    pageLink: "",
-    githubLink: "https://github.com/Fox-Robotics/Line-follower-v1",
+      "Printed Circuit Board (PCB) that redesigns the circuit of Line Follower V1 to optimize space and correct design mistakes to obtain an optimal Line-follower device.",
+    githubLink: "https://github.com/Fox-Robotics/Line-follower-V2.git",
     longDescription:
-      "2Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    technologies: ["C", "KiCAD"],
+      "The Line Follower V2  functions via the QTR-8A Reflectance Sensor Array to detect a black line in the track. This optic sensor sends an analog signal to the Arduino Nano, the Machine Control Unit (MCU). The analog signal received by the MCU is sent again to the TB6612FNG Dual Motor Driver Carrier which consequently controls the two motors on the back wheels. The entire system is powered with a 7.4V Lipo battery.",
+    technologies: ["C++", "KiCAD"],
     technologiesDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+      "Printed circuit board designed using KiCAD and engineered by a C++ code",
     media: [
-      [LineFollowerImg1, "image"],
-      [LineFollowerImg2, "image"],
-      [LineFollowerImg3, "image"],
-    ],
-    members: ["2juanito", "2luis", "2pancho"],
+      [Seguidor V2, "image"],
+    members: ["MIguel Borquez", "Ana Sofia Soto ", "Kevin Payan"],
   },
 ];
 
